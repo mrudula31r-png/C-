38e5ca24a31961b9964f70abf5e6f3e406461d32
Mrudula_R <mrudula31r@gmail.com>
2026-09-07 21:38:52 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a =10;
+    int x = 20;
+    x*=2;
+   cout<< --x+2<< endl;
+    a+=24;
+    cout<< --a <<endl;
+    a%=x;
+    cout<< x%10<<endl;
+    cout<< a++<<endl;
+    --x;
+    a--;
+    cout<< a++ << endl;
+    cout<< --x << endl;
+    int b=10;
+    a/=b;
+    cout<<b++ << endl;
+};
+  
