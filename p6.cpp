236d146516a334c1ef5c06e89b20414eaa5b2e4c
Mrudula_R <mrudula31r@gmail.com>
2026-09-10 21:54:08 +0530

    Implement leap year checker in p6.cpp

--- p6.cpp
+++ p6.cpp
@@ -1,1 +1,14 @@
-
+// Finding weather a year is leap year or not
+#include <iostream>
+using namespace std;
+int main(){
+  int year;
+   cout << "Enter a year: ";
+    cin >> year;
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+      return 0;
+}
